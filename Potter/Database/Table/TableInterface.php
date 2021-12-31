3ac02cf26dda525b6abe0372fb95bb5447a31c3e
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2021-12-31 15:13:34 -0500

    Create TableInterface.php

---
+++ Potter/Database/Table/TableInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Potter\Database\Table;
+
+use Potter\Database\DatabaseInterface;
+
+interface TableInterface
+{
+    public function getDatabase(): DatabaseInterface;
+
+    public function getName(): string;
+
+    public function setDatabase(DatabaseInterface $database): void;
+
+    public function setName(): string;
+}
