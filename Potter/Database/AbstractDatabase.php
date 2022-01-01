2b7bd02a15d4cc82924dc22905efd12bde51521c
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-01 18:00:03 -0500

    Create AbstractDatabase.php

---
+++ Potter/Database/AbstractDatabase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Potter\Database;
+
+use Potter\{
+    Connection\DatabaseConnectionInterface,
+    Dimension\Parent\ParentDimension,
+    //Database\Table\TableInterface
+};
+
+abstract class AbstractDatabase extends ParentDimension implements DatabaseInterface
+{
+    //abstract public function createTable(TableInterface $table): void;
+
+    //abstract public function createTableIfNotExists(TableInterface $table): void;
+    
+    abstract public function getConnection(): DatabaseConnectionInterface;
+
+    //abstract public function getTable(string $table): TableInterface;
+
+    abstract public function getTables(bool $refresh = false): array;
+
+    abstract public function nameIsValid(string $name): bool;
+
+    abstract public function refreshTables(): void;
+
+    abstract public function setConnection(DatabaseConnectionInterface $connection): void;
+
+    abstract public function tableExists(string $table): bool;
+}
