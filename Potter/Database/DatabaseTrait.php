234e47d54003c165742e7883b2acf3fb06373ef2
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-01 18:00:10 -0500

    Create DatabaseTrait.php

---
+++ Potter/Database/DatabaseTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Potter\Database;
+
+use Potter\{
+    Connection\DatabaseConnectionInterface,
+    //Database\Table\TableInterface
+};
+
+trait DatabaseTrait
+{
+    private DatabaseConnectionInterface $connection;
+    private array $tables;
+
+    //abstract public function createTable(TableInterface $table): void;
+
+    /*
+    final public function createTableIfNotExists(TableInterface $table): void
+    {
+        if ($this->tableExists($table->getName())) {
+            return;
+        }
+        $this->createTable($table);
+    }
+    */
+
+    final public function getConnection(): DatabaseConnectionInterface
+    {
+        return $this->connection;
+    }
+
+    abstract public function getName(): string;
+
+    final public function exists(): bool
+    {
+        return $this->connection->databaseExists($this->name);
+    }
+
+    final public function getTables(bool $refresh = false): array
+    {
+        if ($refresh||!isset($this->tables)) {
+            $this->refreshTables();
+        }
+        return $this->tables;
+    }
+
+    final public function refreshTables(): void
+    {
+        $this->tables = $this->connection->getTables($this->name);
+    }
+
+    final public function setConnection(DatabaseConnectionInterface $connection): void
+    {
+        $this->connection = $connection;
+    }
+
+    final public function tableExists(string $table): bool
+    {
+        return in_array(
+            needle: $table,
+            haystack: $this->getTables()
+        );
+    }
+}
