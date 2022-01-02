3f414e00cc9a98f165035312dfac5fed7d6d7e14
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-01 19:20:43 -0500

    Create AbstractTable.php

---
+++ Potter/Database/Table/AbstractTable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Potter\Database\Table;
+
+use Potter\{
+    Database\DatabaseInterface,
+    Dimension\Hybrid\HybridDimension
+};
+
+abstract class AbstractTable extends tHybridDimension implements TableInterface
+{
+    abstract public function getDatabase(): DatabaseInterface;
+
+    abstract public function setDatabase(DatabaseInterface $database): void;
+}
