b92a46efbd9e11755ba0f0c97203e33b1aa8a0f4
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-02 13:00:53 -0500

    Update LocalDatabaseConnection.php

--- Potter/Database/Connection/Local/LocalDatabaseConnection.php
+++ Potter/Database/Connection/Local/LocalDatabaseConnection.php
@@ -2,8 +2,12 @@
 
 namespace Potter\Database\Connection\Local;
 
+use Potter\Database\Connection\DatabaseConnectionTrait;
+
 abstract class LocalDatabaseConnection extends AbstractLocalDatabaseConnection
 {
+    use DatabaseConnectionTrait;
+    
     private const PREFIX = 'localdb';
 
     public function getPrefix(): string
