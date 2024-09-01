34c2fb6885704017d266176803b09fa4d73b8431
Jay Potter <j@ypotter.ca>
2024-09-01 15:23:20 -0400

    Update DatabaseInterface.php

--- src/Potter/Database/DatabaseInterface.php
+++ src/Potter/Database/DatabaseInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Potter\Database;
 
 use Potter\Database\Statement\StatementInterface;
-use Potter\Driver\Database\Aware\DatabaseDriverAwareInterface;
+use Potter\Database\Driver\Aware\DatabaseDriverAwareInterface;
 use Potter\Handle\HandleInterface;
 
 interface DatabaseInterface extends HandleInterface, DatabaseDriverAwareInterface
