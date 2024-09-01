71a460daaaa132e63d168efbea28ff78326e652b
Jay Potter <j@ypotter.ca>
2024-09-01 17:44:00 -0400

    Update AbstractMySQLDriver.php

--- src/Potter/Database/Driver/MySQL/AbstractMySQLDriver.php
+++ src/Potter/Database/Driver/MySQL/AbstractMySQLDriver.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Potter\Database\Driver\MySQL;
 
 use Potter\Database\Driver\AbstractDatabaseDriver;
+use Potter\Database\Result\ResultInterface;
 
 abstract class AbstractMySQLDriver extends AbstractDatabaseDriver implements MySQLDriverInterface
 {
-
+    abstract public function showDatabases(object $handle): ResultInterface;
 }
