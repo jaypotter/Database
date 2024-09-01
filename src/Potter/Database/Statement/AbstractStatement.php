b4a2381d4029b34c5ad9c8e9cb38567e1353cb21
Jay Potter <j@ypotter.ca>
2024-09-01 16:03:44 -0400

    getResult method

--- src/Potter/Database/Statement/AbstractStatement.php
+++ src/Potter/Database/Statement/AbstractStatement.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Potter\Database\Statement;
 
+use Potter\Database\Result\ResultInterface;
+
 abstract class AbstractStatement implements StatementInterface
 {
     abstract public function execute(mixed ...$vars): void;
+    abstract public function getResult(): ResultInterface;
 }
--- src/Potter/Database/Statement/StatementInterface.php
+++ src/Potter/Database/Statement/StatementInterface.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Potter\Database\Statement;
 
+use Potter\Database\Result\ResultInterface;
 use Potter\Stringable\StringableInterface;
 
 interface StatementInterface extends StringableInterface
 {
     public function execute(mixed ...$vars): void;
+    public function getResult(): ResultInterface;
 }
