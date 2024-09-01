11a0375f01f4c53253029a09661e0a975a738834
Jay Potter <j@ypotter.ca>
2024-09-01 17:44:01 -0400

    Update MySQLDriverInterface.php

--- src/Potter/Database/Driver/MySQL/MySQLDriverInterface.php
+++ src/Potter/Database/Driver/MySQL/MySQLDriverInterface.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Potter\Database\Driver\MySQL;
 
 use Potter\Database\Driver\DatabaseDriverInterface;
+use Potter\Database\Result\ResultInterface;
 
 interface MySQLDriverInterface extends DatabaseDriverInterface
 {
-
+    public function showDatabases(object $handle): ResultInterface;
 }
