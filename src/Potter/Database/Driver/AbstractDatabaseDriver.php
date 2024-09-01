31938fcc834760b17f6ccf70c4fbb637bc3b5741
Jay Potter <j@ypotter.ca>
2024-09-01 15:26:26 -0400

    Update AbstractDatabaseDriver.php

--- src/Potter/Database/Driver/AbstractDatabaseDriver.php
+++ src/Potter/Database/Driver/AbstractDatabaseDriver.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Potter\Database\Driver;
 
+use Potter\Driver\AbstractDriver;
 use Potter\Database\Statement\StatementInterface;
 
-abstract class AbstractDatabaseDriver implements DatabaseDriverInterface
+abstract class AbstractDatabaseDriver extends AbstractDriver implements DatabaseDriverInterface
 {
     abstract public function prepare(string $query, object $handle): StatementInterface;
 }
