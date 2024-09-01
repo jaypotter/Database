ad1c7fb87ce0d97b861aba3cf0d8c8264dc3bf7c
Jay Potter <j@ypotter.ca>
2024-09-01 17:43:56 -0400

    Update DatabaseInterface.php

--- src/Potter/Database/DatabaseInterface.php
+++ src/Potter/Database/DatabaseInterface.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Potter\Database;
 
-use Potter\Database\Statement\StatementInterface;
 use Potter\Database\Driver\Aware\DatabaseDriverAwareInterface;
 use Potter\Handle\HandleInterface;
+use Potter\Database\{Statement\StatementInterface, Result\ResultInterface};
 
 interface DatabaseInterface extends HandleInterface, DatabaseDriverAwareInterface
 {
     public function prepare(string $query): StatementInterface;
+    
+    public function getDatabases(): ResultInterface;
 }
