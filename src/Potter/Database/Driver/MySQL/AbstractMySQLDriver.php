b99a0b554494861d962a30976d816ea766a28458
Jay Potter <j@ypotter.ca>
2024-09-01 18:15:38 -0400

    add selectDatabase method

--- src/Potter/Database/Driver/MySQL/AbstractMySQLDriver.php
+++ src/Potter/Database/Driver/MySQL/AbstractMySQLDriver.php
@@ -9,5 +9,6 @@ use Potter\Database\Result\ResultInterface;
 
 abstract class AbstractMySQLDriver extends AbstractDatabaseDriver implements MySQLDriverInterface
 {
+    abstract public function selectDatabase(object $handle): ResultInterface;
     abstract public function showDatabases(object $handle): ResultInterface;
 }
--- src/Potter/Database/Driver/MySQL/MySQLDriverInterface.php
+++ src/Potter/Database/Driver/MySQL/MySQLDriverInterface.php
@@ -9,5 +9,6 @@ use Potter\Database\Result\ResultInterface;
 
 interface MySQLDriverInterface extends DatabaseDriverInterface
 {
+    public function selectDatabase(object $handle): ResultInterface;
     public function showDatabases(object $handle): ResultInterface;
 }
