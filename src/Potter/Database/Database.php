dde743ae5df31c33649a13fb43e4ee2596592f42
Jay Potter <j@ypotter.ca>
2024-09-01 14:27:50 -0400

    Create Database.php

---
+++ src/Potter/Database/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Database;
+
+use Potter\Aware\AwareTrait;
+use Potter\Driver\Aware\DriverAwareTrait;
+use Potter\Driver\Database\{DatabaseDriverInterface, Aware\DatabaseDriverAwareTrait};
+use Potter\Database\Statement\StatementInterface;
+use Potter\Handle\HandleTrait;
+
+final class Database extends AbstractDatabase
+{
+    use AwareTrait, DriverAwareTrait, DatabaseDriverAwareTrait, HandleTrait;
+    
+    public function __construct(?object $handle = null, ?DatabaseDriverInterface $databaseDriver = null)
+    {
+        $this->setHandle($handle);
+        $this->setDatabaseDriver($databaseDriver);
+    }
+    
+    public function prepare(string $query): StatementInterface
+    {
+        
+    }
+}
