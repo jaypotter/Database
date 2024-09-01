6c97906eb8119b51e0a10d7f2d3fbb55eccaacdd
Jay Potter <j@ypotter.ca>
2024-09-01 17:43:53 -0400

    Update AbstractDatabase.php

--- src/Potter/Database/AbstractDatabase.php
+++ src/Potter/Database/AbstractDatabase.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Potter\Database;
 
-use Potter\Database\Statement\StatementInterface;
+use Potter\Database\{Statement\StatementInterface, Result\ResultInterface};
 
 abstract class AbstractDatabase implements DatabaseInterface
 {
     abstract public function prepare(string $query): StatementInterface;
+    
+    abstract public function getDatabases(): ResultInterface;
 }
