e3b6dadec9d39c14c5869e450122485ede74ed4b
Jay Potter <j@ypotter.ca>
2024-09-01 13:25:34 -0400

    Create ResultInterface.php

---
+++ src/Potter/Database/Result/ResultInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Database\Result;
+
+use Potter\ArrayAccess\ArrayAccessInterface;
+
+interface ResultInterface extends ArrayAccessInterface
+{
+
+}
