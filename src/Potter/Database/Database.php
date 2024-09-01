1abcbe72b23295e70e9694cc3ad9b2813e6dea76
Jay Potter <j@ypotter.ca>
2024-09-01 17:43:54 -0400

    Update Database.php

--- src/Potter/Database/Database.php
+++ src/Potter/Database/Database.php
@@ -6,9 +6,10 @@ namespace Potter\Database;
 
 use Potter\Aware\AwareTrait;
 use Potter\Driver\Aware\DriverAwareTrait;
-use Potter\Database\Driver\{DatabaseDriverInterface, Aware\DatabaseDriverAwareTrait};
+use Potter\Database\Driver\{DatabaseDriverInterface, Aware\DatabaseDriverAwareTrait, MySQL\MySQLDriverInterface};
 use Potter\Database\Statement\StatementInterface;
 use Potter\Handle\HandleTrait;
+use Potter\Database\Result\EmptyResult;
 
 final class Database extends AbstractDatabase
 {
@@ -25,5 +26,14 @@ final class Database extends AbstractDatabase
         return $this->getDatabaseDriver()->prepare(
             query: $query,
             handle: $this->getHandle());
+    }
+    
+    public function getDatabases(): ResultInterface
+    {
+        $driver = $this->getDatabaseDriver();
+        if ($driver instanceOf MySQLDriverInterface) {
+            return $driver->showDatabases($this->getHandle());
+        }
+        return new EmptyResult;
     }
 }
