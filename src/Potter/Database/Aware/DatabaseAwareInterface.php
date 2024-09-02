292963e1e4a2da94c48e39be7342f383fb2eca67
Jay Potter <j@ypotter.ca>
2024-09-02 19:35:58 -0400

    Create DatabaseAwareInterface.php

---
+++ src/Potter/Database/Aware/DatabaseAwareInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Database\Aware;
+
+use Potter\Database\DatabaseInterface;
+
+interface DatabaseAwareInterface
+{
+    public function getDatabase(): DatabaseInterface;
+    public function hasDatabase(): bool;
+    public function withDatabase(DatabaseInterface $database): static;
+    public function withoutDatabase(): static;
+}
