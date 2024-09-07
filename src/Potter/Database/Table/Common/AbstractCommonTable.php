9d5fa246d75c5ff8f946b4d1823d14f232d20996
Jay Potter <j@ypotter.ca>
2024-09-07 16:45:07 -0400

    Create AbstractCommonTable.php

---
+++ src/Potter/Database/Table/Common/AbstractCommonTable.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Database\Table\Common;
+
+use Potter\Database\Table\Aware\AbstractTableAware;
+
+abstract class AbstractCommonTable extends AbstractTableAware implements CommonTableInterface
+{
+
+}
