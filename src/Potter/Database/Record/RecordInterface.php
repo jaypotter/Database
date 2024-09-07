1adb7ca849be643164d5ffd9c4d4e9fcef12d7f0
Jay Potter <j@ypotter.ca>
2024-09-07 16:10:55 -0400

    Create RecordInterface.php

---
+++ src/Potter/Database/Record/RecordInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Database\Record;
+
+use Potter\{
+    ArrayAccess\ArrayAccessInterface,
+    Database\Table\Aware\TableAwareInterface
+};
+
+interface RecordInterface extends ArrayAccessInterface, TableAwareInterface
+{
+    public function getValues(): array;
+    public function hasValues(): bool;
+    public function withValues(array $values): static;
+    public function withoutValues(): static;
+    
+    public function getKey(string $key): mixed;
+    public function hasKey(string $key): bool;
+    public function withKey(string $key, ?mixed $value = null): static;
+    public function withoutKey(string $key): static;
+}
