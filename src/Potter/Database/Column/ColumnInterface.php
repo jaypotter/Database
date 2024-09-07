becd0f8b39f60be6f77c0f1bc82e1e93198d92f0
Jay Potter <j@ypotter.ca>
2024-09-07 13:27:52 -0400

    Update ColumnInterface.php

--- src/Potter/Database/Column/ColumnInterface.php
+++ src/Potter/Database/Column/ColumnInterface.php
@@ -6,19 +6,16 @@ namespace Potter\Database\Column;
 
 use Potter\Name\NameInterface;
 
-interface ColumnInterface extends NameInterface
+use Potter\Database\Column\{
+    NotNull\NotNullConstraintInterface,
+    Primary\PrimaryKeyInterface,
+    Type\ColumnTypeInterface,
+    Unique\UniqueConstraintInterface
+};
+
+interface ColumnInterface extends ColumnTypeInterface, NameInterface, NotNullConstraintInterface, PrimaryKeyInterface, UniqueConstraintInterface
 {
-    
-    public function hasNotNullConstraint(): bool;
-    public function withNotNullConstraint(bool $notNull = true): static;
-    public function withoutNotNullConstraint(): static;
-    
-    
-    
-    public function hasUniqueConstraint(): bool;
-    public function withUniqueConstraint(bool $uniqueConstraint = true): static;
-    public function withoutUniqueConstraint(): static;
-    
+   
     public function hasAutoIncrement(): bool;
     public function withAutoIncrement(bool $autoIncrement = true): static;
     public function withoutAutoIncrement(): static;
