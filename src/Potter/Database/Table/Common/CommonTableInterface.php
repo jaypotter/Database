f5c1d627b6e1b36fa3931dbd5a1c36fffa701d25
Jay Potter <j@ypotter.ca>
2024-09-07 16:45:04 -0400

    Create CommonTableInterface.php

---
+++ src/Potter/Database/Table/Common/CommonTableInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Database\Table\Common;
+
+use Potter\Database\Table\Aware\TableAwareInterface;
+
+interface CommonTableInterface extends TableAwareInterface
+{
+
+}
